3b36f4874d9cb7ca10a889206296f5ff4a6ab6f3
BETTlM <sanjith120806@gmail.com>
2025-11-07 14:48:24 +0530

    2025-11-07 14:48:24

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import hashlib
 import dotenv
 
 dotenv.load_dotenv()
-
+print(datetime.datetime)
 HEADERS = {'authorization': 'token '+ os.environ['ACCESS_TOKEN']}
 USER_NAME = os.environ['USER_NAME'] 
 QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 'recursive_loc': 0, 'graph_commits': 0, 'loc_query': 0}
